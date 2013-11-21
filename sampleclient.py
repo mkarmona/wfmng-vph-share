445e58e5de46853ff3cb6bedb6aa53dd1652880e
ecoto <e.coto@sheffield.ac.uk>
2013-11-21 11:34:26 +0000

    Moved the taverna server Id to the config file

--- sampleclient.py
+++ sampleclient.py
@@ -8,16 +8,13 @@ from auth import getAuthTicket
 wfmng = ServerProxy("http://localhost:5000/api")
 user = 'testuser'
 passwd = '6w8DHF'
-tavernaServerASid = "526a92668664882fa4002ba9" # (Taverna server secured - SUN JDK)
-#"526a78538664880543005f6b" # (Taverna server insecured - SUN JDK)
-#"51ded4da86648825040093fe" # (Taverna server secured - OpenJDK)
 ticket = getAuthTicket( user, passwd )
 
 #print "=== getWorkflowsList"
 #print wfmng.getWorkflowsList({}, ticket) # {} is a query
 
 print "=== createTavernaServerWorkflow"
-ret = wfmng.createTavernaServerWorkflow(tavernaServerASid, user, ticket)
+ret = wfmng.createTavernaServerWorkflow(user, ticket)
 
 if ret:
     tavernaServerWorkflowId = ret["workflowId"]
