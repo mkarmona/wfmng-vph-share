4659e5858706d5cf0e7fc30dc067c320f1da2909
ecoto <e.coto@sheffield.ac.uk>
2014-02-26 16:36:22 +0000

    Changed the id for the Taverna Server. Enclosed the workflow submission in a loop to retry the submission when the error is "failed to build workflow run worker".

--- newsampleclient.py
+++ newsampleclient.py
@@ -6,14 +6,8 @@ from auth import getAuthTicket
 import random
 user = ''
 passwd = ''
-# taverna 2.5.1 52d4130086648827c1000ad3
-#taverna secured 2.4.1 528a3b948664880b2b00f0cf
-#tavernaServerCloudId = "528a3b948664880b2b00f0cf"
-#tavernaServerCloudId = "40" # Taverna 2.5 in Prod
-#tavernaServerCloudId = "33" # Taverna 2.4 in Prod
-#tavernaServerCloudId = "5" # Taverna 2.4 in Devel
 
-tavernaServerCloudId = "40"
+tavernaServerCloudId = "223" 
 
 ticket = getAuthTicket( user, passwd )
 
@@ -22,17 +16,24 @@ eid = random.randint(90000, 100000)
 abs_path = os.path.abspath(os.path.dirname(__file__))
 wf_definition = open(os.path.join(abs_path, 'SimpleWorkflow.t2flow'), 'r').read()
 input_definition = open(os.path.join(abs_path, 'SimpleWorkflowInputs.xml'), 'r').read()
-ret = execute_workflow(ticket, eid, "Execution Test %s" % str(eid), tavernaServerCloudId, wf_definition, input_definition)
-if ret=="False":
-    print "=== some errors occurs"
+workflow_worker_built_failed = True
+while workflow_worker_built_failed:
+    ret = execute_workflow(ticket, eid, "Execution Test %s" % str(eid), tavernaServerCloudId, wf_definition, input_definition)
+    if ret=="False":
+       print "=== some errors occurs"
 
-info = getWorkflowInformation(eid, ticket)
-print info
-while info and info['status'] != 'Finished' and info['error'] != True:
-    print "=== sleeping for 5..."
-    time.sleep(5)
-    print "=== getWorkflowInformation"
     info = getWorkflowInformation(eid, ticket)
     print info
-deleteExecution(eid, ticket)
-print "=== Workflow execution deleted from WM"
+    while info and info['status'] != 'Finished' and info['error'] != True:
+        print "=== sleeping for 5..."
+        time.sleep(5)
+        print "=== getWorkflowInformation"
+        info = getWorkflowInformation(eid, ticket)
+        print info
+    deleteExecution(eid, ticket)
+    print "=== Workflow execution deleted from WM"
+    workflow_worker_built_failed = (info['error'] == True) and (info['error_msg']=="Submitting workflow failed failed to build workflow run worker")
+    if workflow_worker_built_failed:
+        print "===  Failed to build workflow run worker, retrying ..."
+        ticket = getAuthTicket( user, passwd ) # renew ticket before retrying
+    
