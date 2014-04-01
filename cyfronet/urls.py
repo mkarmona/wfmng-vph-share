5840e8b3471097c7448a3db60e1c5360d6cdc2e3
Daniel Harezlak <d.harezlak@cyfronet.pl>
2014-04-01 17:13:05 +0200

    lobcder browser search popup fixed and old lobcder resources removed

--- cyfronet/urls.py
+++ cyfronet/urls.py
@@ -10,9 +10,6 @@ urlpatterns = patterns(
     url(r'^datamanager/$', 'datamanager'),
     url(r'^policymanager/$', 'policymanager'),
 
-    url(r'^lobcderquery.*$', 'lobcderSearch'),
-    url(r'^lobcdermetadata(.*)$', 'lobcderMetadata'),
-    url(r'^lobcderdelete(.*)$', 'lobcderDelete'),
     url(r'^lobcder(.*)$', 'lobcder'),
 
 )
